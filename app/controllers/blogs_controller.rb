27fe41121780dd5b59b3533f43e049a78055a5fd
karlley <karlley.jp@gmail.com>
2025-04-16 08:32:11 +0900

    他人のブログの編集画面のアクセス、更新、削除を不可にする

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :require_current_user, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -45,6 +46,12 @@ class BlogsController < ApplicationController
 
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def require_current_user
+    return if current_user == @blog.user
+
+    raise ActiveRecord::RecordNotFound
   end
 
   def blog_params
